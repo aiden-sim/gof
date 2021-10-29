31875319c0bf57b2a3466e03e242286a01bf46e1
aiden.sim <aiden.sim@kakaocorp.com>
2021-10-29 12:47:36 +0900

    memento pattern2

---
+++ src/main/java/behavioral/memento/example/Memento.java
@@ -0,0 +1,13 @@
+package behavioral.memento.example;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
